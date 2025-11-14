661082d88e376a3269d82f0e3dbd382372ce6ef3
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-15 04:12:38 +0500

    task6

---
+++ Tyuiu.AfoninME.Sprint4.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AfoninME.Sprint4.Task6.V27.Lib;
+
+namespace Tyuiu.AfoninME.Sprint4.Task6.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var shapes = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+            int res = ds.Calculate(shapes);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
