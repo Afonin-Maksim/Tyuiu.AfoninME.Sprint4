e951bd401ff914ab79352909f7b39e00552cb4c3
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-15 04:01:56 +0500

    task5

---
+++ Tyuiu.AfoninME.Sprint4.Task5.V24.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AfoninME.Sprint4.Task5.V24.Lib;
+namespace Tyuiu.AfoninME.Sprint4.Task5.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -7, -6, -1, -5, 1 },
+                                          { -1, 1, -1, -1, 6 },
+                                          { -1, 2, 3, 1, -1 },
+                                          { 1, -1, -1, -1, -1 },
+                                          { 1, 0, -1, 1, -1} };
+            int res = ds.Calculate(mas2);
+            int wait = 9;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
