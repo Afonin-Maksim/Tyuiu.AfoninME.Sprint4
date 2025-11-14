4963decf8c919fd1eebb9496bf1701e232354790
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-15 03:25:44 +0500

    task2

---
+++ Tyuiu.AfoninME.Sprint4.Task2.V19/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.AfoninME.Sprint4.Task2.V19.Lib;
+namespace Tyuiu.AfoninME.Sprint4.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Афонин М.Е. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Афонин М.Е. | ИСТНб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 4 до 9 подсчитать сумму нечетных элементов    *");
+            Console.WriteLine("* массива.                                                                *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArry = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                numsArry[i] = rnd.Next(4, 10);
+            }
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numsArry[i] + "\t");
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(numsArry);
+
+            Console.WriteLine("Сумма нечетных элементов  = " + res);
+            Console.ReadKey();
+        }
+    }
+}
